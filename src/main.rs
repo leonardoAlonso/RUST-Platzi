dd06d7982455c2221812ccb65517c06095223bd1
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-06-08 18:07:08 -0500

    pedir datos al usuario y condicionales

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 fn main() {
-    let nombre: &str = "Leonardo";
-    let edad: u8 = 27;
-    println!("Soy {} y mi edad es {} años", nombre, edad);
+    print!("Por favor introduce tu nombre: ");
+    
+    let mut nombre: String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+    // Obtener edad desde consola
+    print!("Por favor introduce tu edad: ");
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+    // convertir la edad a numero
+    let edad_int: u8 = edad.trim().parse().unwrap();
+
+    print!("Hola, bienvenido {} de {} años", nombre, edad_int);
+
+    if edad_int > 18 && edad_int != 30 {
+        print!("Puedes pasar");
+    } else if edad_int == 30 {
+        print!("Tu no");
+    } else {
+        print!("No puedes pasar");
+    }
+
 }
