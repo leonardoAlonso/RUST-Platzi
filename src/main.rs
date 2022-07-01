ae87b2813a41cd8a634a4c5964ded308265b0758
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-06-30 19:37:56 -0500

    vextores y ciclo for

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,23 @@
-use regex::Regex;
-
 fn main() {
 
-   // add regex
-   let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-   // get data from user
+   let mut nombres: Vec<String> = Vec::new();
 
-   println!("Please add your expression: ");
-   let mut expression = String::new();
-   std::io::stdin().read_line(&mut expression).unwrap();
 
-   // apply operations
-   loop{
-    println!("{:?}", expression);
-    let caps = re_add.captures(expression.as_str());
-
-    if caps.is_none() {
-        break;
-    }
-
-    let caps = caps.unwrap();
-
-    let cap_expresion = caps.get(0).unwrap().as_str();
-    let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-    let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-    let addition = left_value + right_value;
-    expression = expression.replace(cap_expresion, &addition.to_string())
-
+   for i in 0..3{
+       println!("Add your name: ");
+       let mut nombre = String::new();
+       std::io::stdin().read_line(&mut nombre).unwrap();
+    
+       nombres.push(nombre);
    }
 
 
-   // show result
+   println!("{:?}", nombres);
+   println!("{}", nombres[0]);
 
-   println!("Result: {}", expression)
+
+   for nombre in nombres{
+    println!("El nombre es: {}", nombre);
+   }
 
 }
